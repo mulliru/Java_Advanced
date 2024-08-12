fcd197888369a03e186f8173840773f257266477
Murillo Ferreira <murilloferreira348@gmail.com>
2024-08-12 19:53:09 -0300

    Atualizando a pasta com o material de aula

---
+++ Primeiro_Semestre/Aula_03/Projeto_Animais/Animais.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.uam.abstracao;
+
+/**
+ *
+ * @author MSWagner
+ */
+public class Animais {
+
+    public static void main (String[] args) {
+        Humano h = new Humano();
+        h.falar();
+
+        Vaca v = new Vaca();
+        v.falar();
+
+        Cachorro c = new Cachorro();
+        c.falar();
+
+        Leao l = new Leao();
+        l.falar();
+    }
+}
---
+++ Primeiro_Semestre/Aula_03/Projeto_Animais/Cachorro.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.uam.abstracao;
+
+/**
+ *
+ * @author MSWagner
+ */
+public class Cachorro extends Animal{
+    @Override
+    public void falar() {
+        System.out.println("Eu posso latir - au au au.");
+    }
+}
