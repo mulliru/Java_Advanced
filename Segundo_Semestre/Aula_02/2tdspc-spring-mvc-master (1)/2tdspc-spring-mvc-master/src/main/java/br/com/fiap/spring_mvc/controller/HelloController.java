7b7b8a6883849b1bf5cf8651babafa4ef8ab3971
Murillo Ferreira <murilloferreira348@gmail.com>
2025-02-18 09:54:21 -0300

    Adicionando update

--- Segundo_Semestre/Aula_02/2tdspc-spring-mvc-master (1)/2tdspc-spring-mvc-master/src/main/java/br/com/fiap/spring_mvc/controller/HelloController.java
+++ Segundo_Semestre/Aula_02/2tdspc-spring-mvc-master (1)/2tdspc-spring-mvc-master/src/main/java/br/com/fiap/spring_mvc/controller/HelloController.java
@@ -7,13 +7,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 public class HelloController {
     @GetMapping("/ola")
     public String hello(Model model) {
         model.addAttribute("message", "Hello World!");
         return "hello"; // Nome do template HTML, ex: hello.html
-
     }
 
     @GetMapping("/ola2")
@@ -29,8 +31,14 @@ public class HelloController {
         livro.setAutor("Homero");
         livro.setTitulo("Odisseia");
         livro.setCategoria(Categoria.ROMANCE);
+        Livro livro2 = new Livro();
+        livro2.setAutor("Homero");
+        livro2.setTitulo("Iliada");
+        livro.setCategoria(Categoria.FATASIA);
+        List<Livro> livros = Arrays.asList(livro, livro2);
         ModelAndView mv = new ModelAndView("livro");
         mv.addObject("livro", livro);
+        mv.addObject("livros", livros);
         return mv;
     }
 
