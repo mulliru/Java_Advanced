4a00049129776be6d5d44d59ed450ef1931436be
Laboratório FIAP <labsfiap@fiap.com.br>
2025-02-28 09:49:44 -0300

    Adicionando a aula

--- Segundo_Semestre/Aula_03/2tdspc-spring-mvc-master/src/main/java/br/com/fiap/spring_mvc/model/Livro.java
+++ Segundo_Semestre/Aula_03/2tdspc-spring-mvc-master/src/main/java/br/com/fiap/spring_mvc/model/Livro.java
@@ -1,26 +1,78 @@
 package br.com.fiap.spring_mvc.model;
 
-import jakarta.persistence.*;
-import lombok.Data;
-
 import java.math.BigDecimal;
 
-@Entity
-@Table(name = "tb_livro")
-@Data
 public class Livro {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    @Column(name = "titulo")
     private String titulo;
-    @Column(name = "autor")
     private String autor;
-    @Column(name = "categoria")
-    @Enumerated(EnumType.STRING)
     private Categoria categoria;
-    @Column(name = "preco")
-    private BigDecimal preco;
-    @Column(name = "isbn")
+    private BigDecimal preco; // Alterado de double para BigDecimal
     private String isbn;
+
+    // Construtor vazio
+    public Livro() {
+    }
+
+    // Construtor com parâmetros
+    public Livro(String titulo, String autor, Categoria categoria, BigDecimal preco, String isbn) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.categoria = categoria;
+        this.preco = preco;
+        this.isbn = isbn;
+    }
+
+    // Getters
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public BigDecimal getPreco() { // Alterado para BigDecimal
+        return preco;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    // Setters
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public void setPreco(BigDecimal preco) { // Alterado para BigDecimal
+        this.preco = preco;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    // Método toString() para facilitar a depuração
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "titulo='" + titulo + '\'' +
+                ", autor='" + autor + '\'' +
+                ", categoria=" + categoria +
+                ", preco=" + preco +
+                ", isbn='" + isbn + '\'' +
+                '}';
+    }
 }
---
+++ Segundo_Semestre/Aula_03/2tdspc-spring-mvc-master/src/main/resources/static/js/script.js
@@ -0,0 +1,7 @@
+function showModal(modalId) {
+    document.getElementById(modalId).style.display = 'block';
+}
+
+function hideModal(modalId) {
+    document.getElementById(modalId).style.display = 'none';
+}
